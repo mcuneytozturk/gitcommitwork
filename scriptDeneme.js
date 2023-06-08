6fe4dab20661b8f073dfc7c302810bf483afd9fb
Muhsin Cüneyt ÖZTÜRK <mcuneytozturkc@gmail.com>
2023-06-08 14:22:26 +0300

    developer branch ekle, scriptDeneme.js ekle

---
+++ scriptDeneme.js
@@ -0,0 +1,15 @@
+let columns = [
+    [1, 2, 3, 4, 5, 6],
+    [1, 2, 3, 4, 5, 6],
+    [1, 2, 3, 4, 5, 6],
+    [1, 2, 3, 4, 5, 6],
+    [1, 2, 3, 4, 5, 6],
+    [1, 2, 3, 4, 5, 6],
+    [1, 2, 3, 4, 5, 6]
+]
+
+for(let row = 0; row < columns.length; row++){
+    let rows = columns[row];
+    for(let i = 0; i < rows.length; i++){
+    }
+}
